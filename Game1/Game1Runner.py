8540645f1cea62a520442961f506afbd9c8303a2
TrumpetDude <johnnydollard@gmail.com>
2016-11-21 18:40:51 -0600

    Major Update
    Compatible with both computers, uses Press Start 2P, changes to color, added developer key to add coins, fixed upgrade bug

--- Game1/Game1Runner.py
+++ Game1/Game1Runner.py
@@ -4,27 +4,25 @@ from random import randint
 
 # Creates the screen to draw on
 pygame.init()
-window = pygame.display.set_mode((1920,1080),pygame.FULLSCREEN)
+window = pygame.display.set_mode((1300,700))#,pygame.FULLSCREEN)
 
 # Allows a key that is held down to count as multiple presses
 pygame.key.set_repeat(1,1)
 
-# Colors the screen white
-red=randint(0,255)
-green=randint(0,255)
-blue=randint(0,255)
-
 #Define Methods
 def windowFill(red,green,blue):
     window.fill((red,green,blue))
-def drawText(text,centerX,centerY):
-    textpos = text.get_rect()
-    textpos.centerx = centerX
-    textpos.centery = centerY
-    window.blit(text, textpos)
+def drawText(text, size, color, centerX, centerY):
+    font=pygame.font.Font("PressStart2P.ttf", size)
+    renderedText=font.render(text,True,color)
+    textpos=renderedText.get_rect()
+    textpos.centerx=centerX
+    textpos.centery=centerY
+    window.blit(renderedText, textpos)
+
+
 
 #Initialize everything
-font=pygame.font.Font(None, 48)
 HP=100
 coins=0
 speed=1
@@ -35,14 +33,14 @@ chest=pygame.image.load("chest.gif")
 chestOpened=pygame.image.load("chestOpened.gif")
 dude=pygame.image.load("dude.gif")
 dudeDamaged = pygame.image.load("dudeDamaged.gif")
-guyX=randint(0,1870)
-guyY=randint(0,1030)
+guyX=randint(0,1250)
+guyY=randint(0,650)
 red=0
 green=0
 blue=0
 HPr=0
 HPg=255
-pygame.mouse.set_visible(False)
+#pygame.mouse.set_visible(False)
 
 #Enemy Stuff
 enemy1OnScreen=False
@@ -59,58 +57,57 @@ zombieDamaged=pygame.image.load("zombieDamaged.gif")
 #enemy1=[10, 2, 0.05]
 #enemy2=[20, 1, 10 (one time, also makes dude into Zombie)
 
+
 # Event Loop
 while True:
 
-    #Draw background and words
+    #Draw Background and words
     windowFill(red,green,blue)
-    pygame.draw.rect(window, (50,50,50), (0,0,250,50),0)
-    pygame.draw.line(window,(0,0,0) ,(105,0), (105,50), 5)
-    font=pygame.font.Font(None, 27)
-    drawText(font.render("+ SPEED: 1", 1, (255, 0, 0)),50,25)
-    drawText(font.render("+ HP REGEN: 2", 1, (255, 0, 0)),175,25)
-    font=pygame.font.Font(None, 48)
-    drawText(font.render("COINS: "+str(coins), 1, (255,255,0)),970,20)
-    drawText(font.render("HP: "+str(int(HP//1)), 1, (HPr, HPg, 0)),970,50)
+    pygame.draw.rect(window, (50,50,50), (0,0,315,50),0)
+    pygame.draw.line(window,(0,0,0) ,(125,0), (125,50), 5)
+    drawText("+ SPEED: 1",12,(0, 0, 255),50,25)
+    drawText("+ HP REGEN: 2",12,(255, 0, 255),220,25)
+    drawText("COINS: "+str(coins), 26, (255,255,0),650,20)
+    drawText("HP: "+str(int(HP//1)), 26, (HPr, HPg, 0),650,50)
     if enemy1OnScreen or enemy2OnScreen:
-        drawText(font.render("TOTAL ENEMY HP: "+str(int(enemy1HP//1)+int(enemy2HP//1)), 1, (255,0,0)),970,80)
-    
+        drawText("TOTAL ENEMY HP: "+str(int(enemy1HP//1)+int(enemy2HP//1)), 26, (255,0,0),650,80)
+
     #Make Coin
     if not(coinOnScreen) and randint(1,500)==1:
         coinOnScreen=True
-        coinX=randint(10,1910)
-        coinY=randint(10,1070)
+        coinX=randint(10,1290)
+        coinY=randint(10,690)
     #Make Chest
     if not(chestOnScreen) and randint(1,10000)==1:
         chestOnScreen=True
-        chestX=randint(10,1820)
-        chestY=randint(10,1020)
-
+        chestX=randint(16,1200)
+        chestY=randint(1,639)
+        
     #Draw Coin
     if coinOnScreen:
         pygame.draw.circle(window, (255,255,0),(coinX,coinY),8,0)
     #Draw Chest
     if chestOnScreen:
         window.blit(chest, (chestX, chestY))
-
+        
     #Pick up Coin
     if coinOnScreen and coinX-guyX<60 and coinX-guyX>-5 and guyY-coinY>-60 and guyY-coinY<10:
         coinOnScreen=False
         coins+=1
-    
 
     #Make Enemy1
     if not(enemy1OnScreen) and randint(1,1000)==1:
         enemy1OnScreen=True
-        enemy1X=randint(0,1334)
-        enemy1Y=randint(0,736)
+        enemy1X=randint(0,1260)
+        enemy1Y=randint(0,660)
         enemy1HP=10
     #Make Enemy2
     if not(enemy2OnScreen) and randint(1,1000)==1:
         enemy2OnScreen=True
-        enemy2X=randint(0,1334)
-        enemy2Y=randint(0,736)
+        enemy2X=randint(0,1260)
+        enemy2Y=randint(0,660)
         enemy2HP=20
+
 
     #Draw person on the screen
     window.blit(dude,(guyX,guyY))
@@ -125,17 +122,14 @@ while True:
         window.blit(dudeDamaged,(guyX,guyY))
         enemy2OnScreen=False
         enemy2HP=0
-
-    #Change HP Text Color
+        
+    #Change HP text color
     if HP>=50:
         HPr=0+5*(100-HP)
     else:
         HPr=255
-        
     if HPr==255:
         HPg=255-5*(50-HP)
-
-    
 
     #Move and draw enemy1
     if enemy1OnScreen:
@@ -175,6 +169,7 @@ while True:
         coins+=10
         enemy1HP=0
         
+
     #Regen HP
     if HP<100-regenSpeed:
         HP+=regenSpeed
@@ -189,7 +184,7 @@ while True:
         prize=randint(1,3)
         coins+=randint(10,50)
         pygame.time.delay(500)
-    
+        
     # Update the screen
     pygame.display.update()
 
@@ -199,20 +194,20 @@ while True:
         
     # Check for key presses
     for event in pygame.event.get():
-        if (event.type==KEYUP and event.key==K_ESCAPE):
+        if (event.type==KEYUP and event.key==K_ESCAPE)or event.type==QUIT:
             pygame.quit()
             sys.exit()
         
         # Check if an arrow key is pressed and 
         # move guy in the correct direction
         elif event.type==KEYDOWN:
-            
-            if event.key==K_w: 
+
+            if event.key==K_w:
                 if guyY>1:
                     guyY-=speed
 
             elif event.key==K_s:
-                if guyY<1032:
+                if guyY<652:
                     guyY+=speed
 
             elif event.key==K_a:
@@ -220,20 +215,24 @@ while True:
                     guyX-=speed
 
             elif event.key==K_d:
-                if guyX<1875:
+                if guyX<1255:
                     guyX+=speed
+
+            elif event.key==K_EQUALS:#DEVELOPER ONLY!
+                coins+=1
+                
                     
             elif event.key==K_1:
-                if coins<speed*speed*speed:
-                    drawText(font.render("NOT ENOUGH COINS! Cost: "+str(speed*speed*speed), 1, (255, 127,0)),970,1000)
+                if speed>=5.999:
+                    drawText("ALREADY AT MAXIMUM VELOCITY!", 24, (255, 0, 0),650,680)
                     pygame.display.update()
-                        
-                elif speed>=5.1:
-                    drawText(font.render("ALREADY AT MAXIMUM VELOCITY!", 1, (255, 0, 0)),970,1000)
+
+                elif coins<speed*speed*speed:
+                    drawText("NOT ENOUGH COINS! Cost: "+str(speed*speed*speed), 24, (255, 127,0),650,680)
                     pygame.display.update()
                         
                 else:
-                    drawText(font.render("Confirm Purchase \"SPEED +1\" for "+str(speed*speed*speed)+" Coins? ENTER/BACKSPACE", 1, (255, 255, 0)),970,1000)
+                    drawText("Confirm Purchase \"SPEED +1\" for "+str(speed*speed*speed)+" Coins? ENTER/BACKSPACE", 20, (0, 0, 255),650,680)
                     pygame.display.update()
                     for event in pygame.event.get():
                         if event.type==KEYDOWN:
@@ -246,17 +245,17 @@ while True:
                                         elif event.key==K_BACKSPACE:
                                             break
 
-            elif event.key==K_2:
-                if coins<int(1000*regenSpeed*1000*regenSpeed*1000*regenSpeed):
-                    drawText(font.render("NOT ENOUGH COINS! Cost: "+str(int(1000*regenSpeed*1000*regenSpeed*1000*regenSpeed)), 1, (255, 127,0)),970,1000)
+            elif event.key==K_2:                       
+                if regenSpeed>=0.005999:
+                    drawText("ALREADY AT MAXIMUM HP REGEN SPEED!", 24, (255, 0, 0),650,680)
                     pygame.display.update()
-                        
-                elif regenSpeed>=0.0051:
-                    drawText(font.render("ALREADY AT MAXIMUM HP REGEN SPEED!", 1, (255, 0, 0)),970,1000)
+
+                elif coins<int(1000*regenSpeed*1000*regenSpeed*1000*regenSpeed):
+                    drawText("NOT ENOUGH COINS! Cost: "+str(int(1000*regenSpeed*1000*regenSpeed*1000*regenSpeed)), 24, (255, 127,0),650,680)
                     pygame.display.update()
-                        
+                    
                 else:
-                    drawText(font.render("Confirm Purchase \"HP REGEN SPEED +1\" for "+str(int(1000*regenSpeed*1000*regenSpeed*1000*regenSpeed))+" Coins? ENTER/BACKSPACE", 1, (255, 255, 0)),970,1000)
+                    drawText("Confirm Purchase \"HP REGEN SPEED +1\" for "+str(int(1000*regenSpeed*1000*regenSpeed*1000*regenSpeed))+" Coins? RETURN/BACKSPACE", 19, (255, 0, 255),650,680)
                     pygame.display.update()
                     for event in pygame.event.get():
                         if event.type==KEYDOWN:
@@ -271,7 +270,7 @@ while True:
 
         elif event.type==KEYUP:
             if event.key==K_SPACE:
-                if enemy1OnScreen and enemy1X-guyX<48 and enemy1X-guyX>-32 and guyY-enemy1Y>-48 and guyY-enemy1Y<32:
+                if enemy1OnScreen and enemy1X-guyX<48 and enemy1X-guyX>-32 and guyY-enemy1Y>-48 and guyY-enemy1Y<32:#Damaging enemy1
                     enemy1HP-=1
                     window.blit(enemy1damaged, (enemy1X,enemy1Y))
                     pygame.display.update()
